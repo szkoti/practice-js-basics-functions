a169622e141e7b2ed66927cfc778c1ddd08f110d
szkoti <120187555+szkoti@users.noreply.github.com>
2024-05-31 11:52:17 +0200

    Hello function

---
+++ 02/app.js
@@ -0,0 +1,18 @@
+// Napisz funkcję o nazwie `sayHello`. Niech przyjmuje ona przez parametr imię osoby, z którą mamy się przywitać.
+
+// Uruchomienie funkcji w sposób `sayHello('devmentor.pl')` ma spowodować wyświetlenie w konsoli napisu:
+//  `Cześć devmentor.pl!`.
+
+
+// function sayHello(name) {
+//     console.log('Hejka ' + name +'!' );
+// }
+
+// sayHello("Marek");
+
+
+const sayHello = name => {
+    console.log('Hejka ' + name +'!' );
+}
+
+sayHello("Foczka");
